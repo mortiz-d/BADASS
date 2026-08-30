35ba0192a9a8e994d6618f5fab4289f4f560c878
Miguel Angel Ortiz del Burgo <mortiz@singularaircraft.com>
2026-08-30 18:33:46 +0200

    Seems functional , i wonder if i have to add the gn3s repo too to this

--- p1/router/config/router-entrypoint.sh
+++ p1/router/config/router-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 set -e
 
-# In case configfile comes with other permits , frr ignores this file if thats the case
-chown frr:frr /etc/frr/frr.conf 2>/dev/null || true
-chmod 640 /etc/frr/frr.conf 2>/dev/null || true
-
 /usr/lib/frr/frrinit.sh start
 
 exec tail -f /dev/null
